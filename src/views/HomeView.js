c55d0d0983ace6cf67324f51a26bd4acb925aa33
cr1m3s <parioleks@gmail.com>
2022-09-04 19:10:20 +0200

    pr

---
+++ src/views/HomeView.js
@@ -0,0 +1,13 @@
+import Title from '../Components/Title';
+import Trending from '../Components/Trending';
+
+const HomeView = () => {
+  return (
+    <>
+      <Title title="Trending today" />
+      <Trending />
+    </>
+  );
+};
+
+export default HomeView;
